963e6e70a70afc6a12ec6c8259be0b2c6ef7c18e
Goshgar <qosqarmirze@gmail.com>
2020-11-01 03:12:03 +0400

    changes

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="/favicon.ico"><title>qr-menu</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script><style>#loader {
+<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="/favicon.ico"><title>qr-menu</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script><link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:wght@400;700&display=swap" rel="stylesheet"><style>#loader {
   position: absolute;
   left: 50%;
   top: 50%;
@@ -6,13 +6,13 @@
   width: 150px;
   height: 150px;
   margin: -75px 0 0 -75px;
-  border: 16px solid #f3f3f3;
+  /* border: 16px solid #f3f3f3; */
   border-radius: 50%;
-  border-top: 16px solid #3498db;
+  /* border-top: 16px solid #3498db; */
   width: 120px;
   height: 120px;
-  -webkit-animation: spin 2s linear infinite;
-  animation: spin 2s linear infinite;
+  
+  /* animation: spin 2s linear infinite; */
 }
 
 @-webkit-keyframes spin {
@@ -42,4 +42,64 @@
 @keyframes animatebottom { 
   from{ bottom:-100px; opacity:0 } 
   to{ bottom:0; opacity:1 }
-}</style><link href="/css/app.79cc5058.css" rel="preload" as="style"><link href="/js/app.f21bdf57.js" rel="preload" as="script"><link href="/js/chunk-vendors.a57160a3.js" rel="preload" as="script"><link href="/css/app.79cc5058.css" rel="stylesheet"></head><body><div id="loader"></div><div id="app" style="display: none;"></div><script src="/js/chunk-vendors.a57160a3.js"></script><script src="/js/app.f21bdf57.js"></script></body></html><script></script>
+}
+  .preloader {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999999999;
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    overflow: hidden;
+}
+.preloader-inner {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate(-50%,-50%);
+    -moz-transform: translate(-50%,-50%);
+    transform: translate(-50%,-50%);
+}
+.preloader .preloader-circle {
+    width: 100px;
+    height: 100px;
+    position: relative;
+    border-style: solid;
+    border-width: 1px;
+    border-top-color: rgb(51, 143, 8);
+    border-bottom-color: transparent;
+    border-left-color: transparent;
+    border-right-color: transparent;
+    z-index: 10;
+    border-radius: 50%;
+    -webkit-box-shadow: 0 1px 5px 0 rgba(35,181,185,0.15);
+    box-shadow: 0 1px 5px 0 rgba(35,181,185,0.15);
+    background-color: #fff;
+    /* -webkit-animation: zoom 2000ms infinite ease; */
+    -webkit-animation: spin 2s linear infinite;
+    /* animation: zoom 2000ms infinite ease; */
+    -webkit-transition: .6s;
+    -o-transition: .6s;
+    transition: .6s;
+}
+.preloader .preloader-img {
+    position: absolute;
+    top: 50%;
+    z-index: 200;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    text-align: center;
+    display: inline-block;
+    -webkit-transform: translateY(-50%);
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
+    padding-top: 6px;
+    -webkit-transition: .6s;
+    -o-transition: .6s;
+    transition: .6s;
+}
+.preloader .preloader-img img {
+    max-width: 55px;
+}</style><link href="/css/app.79eff07b.css" rel="preload" as="style"><link href="/js/app.85aae108.js" rel="preload" as="script"><link href="/js/chunk-vendors.77ba0bed.js" rel="preload" as="script"><link href="/css/app.79eff07b.css" rel="stylesheet"></head><body><div id="loader"><div id="preloader-active" style="display: block;"><div class="preloader d-flex align-items-center"><div class="preloader-inner"><div class="preloader-circle"></div><div class="preloader-img pere-text"><img src="http://qrtavada.resanthotels.az/assets/img/logo/logo.png" alt=""></div></div></div></div></div><div id="app" style="display: none;"></div><script src="/js/chunk-vendors.77ba0bed.js"></script><script src="/js/app.85aae108.js"></script></body></html><script></script>
